581ffdc9805fa1ce2b10ecb01472c15135b12560
Your Name <you@example.com>
2019-10-10 07:47:55 +0000

    Vue js music_application

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!-- HTML part -->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <HelloWorld msg="Welcome to your music  app" />
+  </div>
+</template>
+
+
+<!-- Java Script part-->
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+
+<!-- CSS part here-->
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
